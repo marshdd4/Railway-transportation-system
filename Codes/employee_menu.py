de08d1a86062aa8ecca75c8b632850231de847a6
hosein <aghayshab.2018@gmail.com>
2026-02-17 22:13:02 +0330

    debug menu

--- Codes/employee_menu.py
+++ Codes/employee_menu.py
@@ -1,19 +1,28 @@
 from add_employee import emp
 from employee_panel import emp_panel
+
 def emp_menu():
     while True:
         print("** Welcome **")
         username = input("Enter your username:")
         password = input("Enter your password:")
-        if emp:
-            for i in emp:
-                if i["user_name"] == username and i["pass_word"] == password:
-                    print("Login Successful!")
-                    emp_panel()
-                else:
-                    print("Incorrect username or password")
-        else:
+
+        if not emp:
             print("No employees available!")
+            return   
+
+        for i in emp:
+            if i["user_name"] == username and i["pass_word"] == password:
+                print("Login Successful!")
+                emp_panel()
+                return 
+        print("Incorrect username or password")
+
         reeturn = input("Enter *0* to return / Enter *1* to try again: ")
-        if reeturn=="0":
+
+        if reeturn == "0":
             return
+
+
+        
+
--- Codes/employee_panel.py
+++ Codes/employee_panel.py
@@ -1,51 +1,43 @@
 from Train_employee import Train_emp
-
-def get_first_menu():
-    from start_menu import first_menu
-    first_menu()
-
+empo = Train_emp()
 def emp_panel():
-    empo = Train_emp()
-    while True:   
-        print("** Welcome to Employee Panel **")
-        print("1. Add line")
-        print("2. Update info")
-        print("3. Delete line")
-        print("4. Show line list")
-        print("5. Add train")
-        print("6. Update train info")
-        print("7. Delete train")
-        print("8. Show train list")
-        print("9. Exit")
-            
-        choice=input("Please Enter your choice(1-9):" )
-
+    while True:
+        print("**welcome to employee menu**")
+        print("1. add line")
+        print("2. update info")
+        print("3. delete line")
+        print("4. show line list")
+        print("5. add train")
+        print("6. update train info")
+        print("7. delete train")
+        print("8. show train list")
+        print("9. exit")
+        
+        choice=input("please inter your choice(1-9):" )
+    
         if choice=="1":
             empo.add_line()
-                            
+            
         elif choice=="2":
             empo.update_info()
-                        
+        
         elif choice=="3":
             empo.delete_line()
-                    
+        
         elif choice=="4":
             empo.show_line_list()
-                        
+        
         elif choice=="5":
             empo.add_train()
-                    
+        
         elif choice=="6":
             empo.update_train_info()
-                        
+        
         elif choice=="7":
             empo.delete_train()
-
+    
         elif choice=="8":
             empo.show_train_list()
-                            
+            
         elif choice=="9": 
-            get_first_menu()
-        else:
-            print("Invalid choice!")
-            emp_panel()
+            return  
